25a72c61d0d1345ae61c5961aacab5f72f5b3cdb
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-09-01 15:26:04 -0600

    added a few utilities and notes

--- src/thx/stream/Producer.hx
+++ src/thx/stream/Producer.hx
@@ -1,7 +1,9 @@
 package thx.stream;
 
 import thx.core.Error;
+import thx.core.Nil;
 import thx.promise.Promise;
+import haxe.ds.Option;
 
 class Producer<T> {
   var handler : Signer<T> -> (Void -> Void);
@@ -112,6 +114,27 @@ class Producer<T> {
         handler
       );
     });
+  public function toOption() : Producer<Option<T>>
+    return mapValue(function(v) return null == v ? None : Some(v));
+  public function toNil() : Producer<Nil>
+    return mapValue(function(_) return nil);
+  public function toTrue() : Producer<Bool>
+    return mapValue(function(_) return true);
+  public function toFalse() : Producer<Bool>
+    return mapValue(function(_) return false);
+  public function log(?prefix : String, ?posInfo : haxe.PosInfos) {
+    prefix = prefix == null ? '': '${prefix}: ';
+    return mapValue(function(v) {
+      haxe.Log.trace('$prefix$v', posInfo);
+      return v;
+    });
+  }
+
+  public function withValue(?expected : T) : Producer<T>
+    if(null == expected)
+      return filterValue(function(v : T) return v != null)
+    else
+      return filterValue(function(v : T) return v == expected);
 
   function passOn(?pulse : T -> Void, ?failure : Error -> Void, ?end : Void -> Void, handler : StreamValue<T> -> Void)
     return subscribe(
@@ -119,8 +142,100 @@ class Producer<T> {
         null != failure ? failure : function(e : Error) handler(Failure(e)),
         null != end     ? end     : function()  handler(End)
       );
-
+// blend
+// keep
+// debounce
+// sampleBy
+// pair
+// distinct
 // merge
+// concat
 // sync
 // zip
+// previous
+// public function window(length : Int, fillBeforeEmit = false) : Producer<T> // or unique
+// public function reduce(acc : TOut, TOut -> T) : Producer<TOut>
+// public function debounce(delay : Int) : Producer<T>
+// exact pair
+// public function zip<TOther>(other : Producer<TOther>) : Producer<Tuple<T, TOther>> // or sync
+
+// mapFilter?
+
+/*
+  public static function filterOption<T>(producer : Producer<Option<T>>) : Producer<T>
+    return producer
+      .filter(function(opt) return switch opt { case Some(_): true; case None: false; })
+      .map(function(opt) return switch opt { case Some(v) : v; case None: throw 'filterOption failed'; });
+
+  public static function toValue<T>(producer : Producer<Option<T>>) : Producer<Null<T>>
+    return producer
+      .map(function(opt) return switch opt { case Some(v) : v; case None: null; });
+
+  public static function toBool<T>(producer : Producer<Option<T>>) : Producer<Bool>
+    return producer
+      .map(function(opt) return switch opt { case Some(_) : true; case None: false; });
+
+  public static function skipNull<T>(producer : Producer<Null<T>>) : Producer<T>
+    return producer
+      .filter(function(value) return null != value);
+
+  public static function left<TLeft, TRight>(producer : Producer<Tuple2<TLeft, TRight>>) : Producer<TLeft>
+    return producer.map(function(v) return v._0);
+
+  public static function right<TLeft, TRight>(producer : Producer<Tuple2<TLeft, TRight>>) : Producer<TRight>
+    return producer.map(function(v) return v._1);
+
+  public static function negate(producer : Producer<Bool>)
+    return producer.map(function(v) return !v);
+
+  public static function flatMap<T>(producer : Producer<Array<T>>) : Producer<T> {
+    return new Producer(function(forward : Pulse<T> -> Void) {
+      producer.feed(Bus.passOn(
+        function(arr : Array<T>) arr.map(function(value) forward(Emit(value))),
+        forward
+      ));
+    }, producer.endOnError);
+  }
+
+  public static function delayed<T>(producer : Producer<T>, delay : Int) : Producer<T> {
+    return new Producer(function(forward) {
+      producer.feed(new Bus(
+        function(v)
+          Timer.setTimeout(function() forward(Emit(v)), delay),
+        function()
+          Timer.setTimeout(function() forward(End), delay),
+        function(error)
+          Timer.setTimeout(function() forward(Fail(error)), delay)
+      ));
+    }, producer.endOnError);
+  }
+
+@:access(steamer.Producer)
+class ProducerProducer {
+  public static function flatMap<T>(producer : Producer<Producer<T>>) : Producer<T> {
+    return new Producer(function(forward : Pulse<T> -> Void) {
+      producer.feed(Bus.passOn(
+        function(prod : Producer<T>) {
+          prod.feed(Bus.passOn(
+            function(value : T) forward(Emit(value)),
+            forward
+          ));
+        },
+        forward
+      ));
+    }, producer.endOnError);
+  }
+}
+
+class StringProducer {
+  public static function toBool(producer : Producer<String>) : Producer<Bool>
+    return producer
+      .map(function(s) return s != null && s != "");
+}
+*/
+  public static function ofArray<T>(arr : Array<T>)
+    return new Producer(function(handler) {
+      arr.map(function(v) handler(Pulse(v)));
+      return function(){};
+    });
 }
