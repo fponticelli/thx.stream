bc4914f65451c5d4f5e4991955e5a98b439c7296
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-09-28 18:21:17 -0600

    added API doc generation

---
+++ doc/ImportStream.hx
@@ -0,0 +1,10 @@
+import thx.stream.Bus;
+import thx.stream.Emitter;
+import thx.stream.IStream;
+import thx.stream.Stream;
+import thx.stream.Streams;
+import thx.stream.StreamValue;
+#if (js || flash)
+import thx.stream.Timer;
+#end
+import thx.stream.Value;
