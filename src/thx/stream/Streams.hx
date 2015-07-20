4799f0e5ef7d509a8fd2671a2abf54dfb0d4fedd
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-07-19 19:19:01 -0600

    added StreamArrays.toStream

--- src/thx/stream/Streams.hx
+++ src/thx/stream/Streams.hx
@@ -1,5 +1,7 @@
 package thx.stream;
 
+using thx.promise.Promise;
+
 class Streams {
   public static function toStream<T>(values : Array<T>) : Emitter<T> {
     return new Emitter(function(stream) {
@@ -8,6 +10,24 @@ class Streams {
         stream.pulse(value);
       }
       stream.end();
+    });
+  }
+}
+
+class StreamArrays {
+  public static function toStream<T>(promise : Promise<Array<T>>) : Emitter<T> {
+    return new Emitter(function(stream) {
+      promise
+        .success(function(values) {
+          for(value in values) {
+            if(stream.canceled) return;
+            stream.pulse(value);
+          }
+          stream.end();
+        })
+        .failure(function(err) {
+          throw err;
+        });
     });
   }
 }
