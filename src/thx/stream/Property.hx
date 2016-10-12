3d534ff4974bae9062f751973b89fad5008fb4b2
Franco Ponticelli <franco.ponticelli@gmail.com>
2016-10-12 09:43:31 -0600

    notes and renames

--- src/thx/stream/Property.hx
+++ src/thx/stream/Property.hx
@@ -7,17 +7,17 @@ class Property<T> {
   public var stream(default, null): Stream<T>;
   public var value(default, null): T;
 
-  var equality: T -> T -> Bool;
+  var equals: T -> T -> Bool;
   var emitter: StreamEmitter<T>;
-  public function new(initial: T, ?equality: T -> T -> Bool) {
+  public function new(initial: T, ?equals: T -> T -> Bool) {
     this.emitter = Emitter.last();
     this.stream = emitter.stream;
-    this.equality = null == equality ? Functions.equality : equality;
+    this.equals = null == equals ? Functions.equality : equals;
     set(initial);
   }
 
   public function set(value: T) {
-    if(equality(this.value, value))
+    if(equals(this.value, value))
       return;
     this.value = value;
     emitter.next(value);
